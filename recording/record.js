c03de3d4cbc9673844fa1a0890674d0045095b55
Kanai <u606601k@gmail.com>
2020-11-23 23:39:04 +0900

    s3のアップロード先を変更

--- recording/record.js
+++ recording/record.js
@@ -5,6 +5,7 @@ const { spawn } = require('child_process');
 const { S3Uploader } = require('./utils/upload');
 
 const MEETING_URL = process.env.MEETING_URL || 'Not present in environment';
+const RECORDING_ID = process.env.RECORDING_ID || 'Not present in environment';
 console.log(`[recording process] MEETING_URL: ${MEETING_URL}`);
 
 const args = process.argv.slice(2);
@@ -73,7 +74,8 @@ const year = timestamp.getFullYear();
 const month = timestamp.getMonth() + 1;
 const day = timestamp.getDate();
 const hour = timestamp.getUTCHours();
-const fileName = `${year}/${month}/${day}/${hour}/${fileTimestamp}.mp4`;
+const defaultFileName = `${year}/${month}/${day}/${hour}/${fileTimestamp}`;
+const fileName = `${RECORDING_ID || defaultFileName}.mp4`;
 new S3Uploader(BUCKET_NAME, fileName).uploadStream(transcodeStreamToOutput.stdout);
 
 // event handler for docker stop, not exit until upload completes
